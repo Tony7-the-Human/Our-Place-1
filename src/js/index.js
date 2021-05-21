2c331d81d414443dfa335e40d5ba3d2a3117d2cd
Tony7-the-Human <mrfox88@yandex.ru>
2021-05-21 20:08:29 +0300

    add new sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 import "./import/modules";
+
+
+
+let colorItems = document.querySelectorAll('.design-color__item-circle');
+let active = colorItems[0]; // moo[0], чтобы при первом клике не было ошибки.
+
+for (let i = 0; i < colorItems.length; i++) {
+    colorItems[i].addEventListener('click', function() {
+        if (active == this) {
+            // Если active ссылается на тот же объект, что и this (на кликнутую кнопку)
+            this.classList.toggle('active');
+        } else {
+            active.classList.remove('active');
+            this.classList.add('active');
+
+            active = this;
+        }
+    });
+}
