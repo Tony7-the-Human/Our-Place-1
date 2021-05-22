34dbdbdac04fb23b506919a6fbf8ff183a477495
Tony7-the-Human <mrfox88@yandex.ru>
2021-05-22 22:39:26 +0300

    add section goods

--- src/js/index.js
+++ src/js/index.js
@@ -18,3 +18,34 @@ for (let i = 0; i < colorItems.length; i++) {
         }
     });
 }
+
+
+let colorItemsMin = document.querySelectorAll('.good-color__item-circle');
+let activeMin = colorItemsMin[0]; // moo[0], чтобы при первом клике не было ошибки.
+
+for (let i = 0; i < colorItemsMin.length; i++) {
+    colorItemsMin[i].addEventListener('click', function() {
+        if (activeMin == this) {
+            // Если active ссылается на тот же объект, что и this (на кликнутую кнопку)
+            this.classList.toggle('active');
+        } else {
+            activeMin.classList.remove('active');
+            this.classList.add('active');
+
+            activeMin = this;
+        }
+    });
+}
+
+
+function ibg() {
+
+    let ibg = document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+        }
+    }
+}
+
+ibg();
